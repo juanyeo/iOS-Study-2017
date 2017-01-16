c6bd0164beab7ae583fa078cacae017252ef5ce1
여주안 <JuanYeo@JongWonui-MacBook-Air.local>
2017-01-16 14:04:10 +0900

    AntFunction

--- ConvertTemperature/ConvertTemperature/ViewController.swift
+++ ConvertTemperature/ConvertTemperature/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputBox: UITextField!
+    @IBOutlet weak var fromC: UIButton!
+    @IBOutlet weak var fromF: UIButton!
+    @IBOutlet weak var resultBox: UITextField!
+    var load = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
-
+    
+    @IBAction func fromCTouched(_ sender: Any) {
+        let inputC = Double(inputBox.text!)
+        var street1 = Measurement(value: inputC!, unit: UnitTemperature.celsius)
+        street1.convert(to: UnitTemperature.fahrenheit)
+        resultBox.text = "\(street1.value)℉"
+    }
+    @IBAction func fromFTouched(_ sender: Any) {
+        let inputF = Double(inputBox.text!)
+        var street2 = Measurement(value: inputF!, unit: UnitTemperature.fahrenheit)
+        street2.convert(to: UnitTemperature.celsius)
+        resultBox.text = "\(street2.value)℃"
+    }
+    }
--- Day7/Day7/main.swift
+++ Day7/Day7/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let fileM = FileManager()
-
+let load = busyAnt()
+print(load.antFunction(inputArray: [1,2,3,3,2,2,2,1]))
 
 
